5b23279dc9c0380a406dbc6f0698f24b2557edbb
Colum Ferry <cferry09@gmail.com>
2021-12-01 10:43:55 +0000

    add auth service

---
+++ libs/shared/auth/src/lib/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private _activeUser = new Subject<{ username: string }>();
+
+  activeUser = this._activeUser.asObservable();
+
+  login({ username, password }: { username: string; password: string }) {
+    if (password === 'password') {
+      this._activeUser.next({ username });
+    }
+  }
+}
